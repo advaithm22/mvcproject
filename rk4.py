75682375b08a3a5b990f302d5202162110bc3f6a
Advaith Modali <80607809+advaithm22@users.noreply.github.com>
2022-02-06 22:19:48 -0800

    added graphs

--- rk4.py
+++ rk4.py
@@ -1,71 +1,44 @@
-from fractions import Fraction
+import math 
+import matplotlib.pyplot as plt
 
-#ordinary differential equation in the form: dy/dx = x + 1/2y
-diffeq = lambda x,y: (x-y)/2
+#ordinary differential equation
+diffeq = lambda x,y: -1/(x-5.51)**2
+#analytical function
+f = lambda x: 1/(x-5.51)
 
-#Table
-table = dict()
-table['c1']= []
-table['c2']= [1/2,1/2]
-table['c3']= [1/2,0,1/2]
-table['c4']=[1,0,0,1]
+#Since there may be errors with allowing the user to input starting conditions and step sizes, 
+#there will be a section of the code dedicated to using pure provided values for the RK4 algorithm.
 
-def printTable(order):
-    #Include code to print the table
-    if order == 2:
-        print('c1' + '|' + '""')
-        print(str(table['c2'][0]) + '|' + str(table['c2'][1]))
-    elif order == 3:
-        print('c1' + '|' + '""')
-        print(str(table['c2'][0]) + '|' + str(table['c2'][1]))
-        print(str(table['c3'][0]) + '|' + str(table['c3'][1]) + ' '+ str(table['c3'][2]))
-    else:
-        print('c1' + '|' + '""')
-        print(str(table['c2'][0]) + '|' + str(table['c2'][1]))
-        print(str(table['c3'][0]) + '|' + str(table['c3'][1]) + ' '+ str(table['c3'][2]))
-        print(str(table['c4'][0]) + '|' + str(table['c4'][1]) + ' '+ str(table['c4'][2])+ ' '+ str(table['c4'][3]))
-        
+xi = 0
+xf = 10
+h = 1
+s = math.ceil((xf-xi)/h)
+x = 0
+y = 2
+print(x)
+print(math.e)
+type(x)
 
+x_plt = []
+y_pred = []
+y_an = []
 
-def rk4(x0,y0,dx,x,order):
+for i in range(1,s+1):
+  k1 = diffeq(x,y)
+  k2 = diffeq(x + h/2, y + k1 * h/2)
+  k3 = diffeq(x + h/2, y + k2 * h/2)
+  k4 = diffeq(x + h, y + k3 *h)
+  y += (1.0/6.0) * (k1 + 2*k2 + 2*k3 + k4) * h
+  x_plt.append(x)
+  y_pred.append(y) 
+  y_an.append(f(x))
+  x += h
 
-    s = int((x-x0)/dx)
-
-    if order == 0 or order ==1 or order > 4:
-        print ("The minimum order needs to be 2, 3 or 4")
-    elif order == 2:
-        #call the printtable with order argument
-        printTable(order)
-        for i in range(1,s+1):
-            k1 = diffeq(x0,y0) * dx
-            k2 = diffeq(x0 + table['c2'][0] * dx,y0 + k1 * table['c2'][1]) * dx
-            x0+=dx
-        y_new = y0 + (1.0/6.0) * (k1 + k2)
-    elif order == 3:
-        #call the printtable with order argument
-        printTable(order)
-        for i in range(1,s+1):
-            k1 = diffeq(x0,y0) * dx
-            k2 = diffeq(x0 + table['c2'][0] * dx,y0 + k1 * table['c2'][1]) * dx
-            k3 = diffeq(x0 + table['c3'][0] * dx,y0 +(k1 * table['c3'][1])+ (k2 * table['c3'][2])) * dx
-            x0+=dx
-        y_new = y0 + (1.0/6.0) * (k1 + 2*k2 + k3)
-    else:
-        #call the printtable with order argument
-        printTable(order)
-        for i in range(1,s+1):
-            k1 = diffeq(x0,y0) * dx
-            k2 = diffeq(x0 + table['c2'][0] * dx,y0 + k1 * table['c2'][1]) * dx
-            k3 = diffeq(x0 + table['c3'][0] * dx,y0 +(k1 * table['c3'][1])+ (k2 * table['c3'][2])) * dx
-            k4 = diffeq(x0 + table['c4'][0] * dx,y0 + (k1 * table['c4'][1])+ (k2 * table['c4'][2]))  * dx
-            x0+=dx
-        y_new = y0 + (1.0/6.0) * (k1 + 2*k2 + 2*k3 + k4)
-    return y_new
-
-
-#user inputs
-x0 = float(input("Please enter the starting point of x you would like to compute for:"))
-y0 = float(input("Please enter the starting point of y you would like to compute for:"))
-dt = float(input("Please enter the step-size, or Δx value you would like to use:"))
-x = float(input("Please enter the final x value you would like to use the Runge-Kutta Method to approximate:"))
-print("The value of the function at the x-value you gave is", rk4(x0,y0,dt,x))
+print(x_plt)
+print(y_pred)
+f_lambda = f(x)
+print(f_lambda)
+plt.plot(x_plt,y_pred, 'bo', x_plt, y_an, 'r+')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend(["Code", 'Analytic'])
